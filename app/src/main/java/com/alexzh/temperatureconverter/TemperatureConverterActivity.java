e33f24c18adee26644b2377d6d0928b5933ea269
Alex <zhukovich.a@gmail.com>
2016-08-20 22:07:49 +0200

    Updated activity; using View and Presenter;

--- app/src/main/java/com/alexzh/temperatureconverter/TemperatureConverterActivity.java
+++ app/src/main/java/com/alexzh/temperatureconverter/TemperatureConverterActivity.java
@@ -9,16 +9,26 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.alexzh.temperatureconverter.di.TemperatureConverterApp;
+import com.alexzh.temperatureconverter.model.Temperature;
+
+import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class TemperatureConverterActivity extends AppCompatActivity {
+public class TemperatureConverterActivity extends AppCompatActivity implements TemperatureConverterView {
+    private final static int CELSIUS_POSITION = 0;
+    private final static int FAHRENHEIT_POSITION = 1;
+    private final static int KELVIN_POSITION = 2;
+
 
     @BindView(R.id.inputView) AppCompatEditText mInputView;
     @BindView(R.id.inputTemperatureSpinner) AppCompatSpinner mInputUnitSpinner;
     @BindView(R.id.outputView) TextView mIOutputView;
     @BindView(R.id.outputTemperatureSpinner) Spinner mOutputUnitSpinner;
+
+    @Inject TemperatureConverterPresenter mPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +43,56 @@ public class TemperatureConverterActivity extends AppCompatActivity {
         temperatureUnitAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mInputUnitSpinner.setAdapter(temperatureUnitAdapter);
         mOutputUnitSpinner.setAdapter(temperatureUnitAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mPresenter.attachView(this);
+    }
+
+    @OnClick(R.id.convertButton)
+    public void onConvertButtonClicked() {
+        if (mPresenter != null) {
+            mPresenter.convertTemperature();
+        }
+    }
+
+    @Override
+    public String getInputValue() {
+        return mInputView.getText().toString();
+    }
+
+    @Override
+    public Temperature getFromTemperatureUnit() {
+        return getTemperatureUnitFromSpinner(mInputUnitSpinner);
+    }
+
+    private Temperature getTemperatureUnitFromSpinner(Spinner spinner) {
+        switch (spinner.getSelectedItemPosition()) {
+            case FAHRENHEIT_POSITION:
+                return Temperature.FAHRENHEIT;
+            case KELVIN_POSITION:
+                return Temperature.KELVIN;
+            case CELSIUS_POSITION:
+            default:
+                return Temperature.CELSIUS;
+        }
+    }
+
+    @Override
+    public Temperature getToTemperatureUnit() {
+        return getTemperatureUnitFromSpinner(mOutputUnitSpinner);
+    }
+
+    @Override
+    public void setOutputValue(double value) {
+        mIOutputView.setText(getString(R.string.output_text_format, value));
+    }
+
+    @Override
+    protected void onStop() {
+        mPresenter.detachView();
+        super.onStop();
     }
 }
