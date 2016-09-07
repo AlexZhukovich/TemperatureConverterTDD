7cd1fb3cd8eefc8f582d477966dad21f07264ee1
Alex <zhukovich.a@gmail.com>
2016-09-07 23:17:36 +0200

    Update the ConvertTemperatureRepository

--- app/src/main/java/com/alexzh/temperatureconverter/calculation/ConvertTemperatureRepository.java
+++ app/src/main/java/com/alexzh/temperatureconverter/calculation/ConvertTemperatureRepository.java
@@ -1,9 +1,8 @@
 package com.alexzh.temperatureconverter.calculation;
 
-import com.alexzh.temperatureconverter.model.ConvertedResult;
 import com.alexzh.temperatureconverter.model.InputData;
 
 public interface ConvertTemperatureRepository {
 
-    ConvertedResult convertData(InputData inputData);
+    void convertData(InputData inputData);
 }
