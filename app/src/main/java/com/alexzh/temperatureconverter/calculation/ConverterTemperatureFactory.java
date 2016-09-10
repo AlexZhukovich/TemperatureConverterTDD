2a29626a776f53b5cedb08a98ccddde1de3cff2b
Alex <zhukovich.a@gmail.com>
2016-09-10 13:48:18 +0200

    Implemented ConverterTemperatureFactory

---
+++ app/src/main/java/com/alexzh/temperatureconverter/calculation/ConverterTemperatureFactory.java
@@ -0,0 +1,32 @@
+package com.alexzh.temperatureconverter.calculation;
+
+import com.alexzh.temperatureconverter.calculation.offline.OfflineConvertTemperature;
+import com.alexzh.temperatureconverter.calculation.online.OnlineConverterTemperature;
+import com.alexzh.temperatureconverter.calculation.online.TemperatureConverterApiService;
+import com.alexzh.temperatureconverter.service.SharedPreferenceManager;
+
+import org.greenrobot.eventbus.EventBus;
+
+import javax.inject.Inject;
+
+public class ConverterTemperatureFactory {
+    private SharedPreferenceManager mPreferenceManager;
+
+    @Inject TemperatureConverterApiService mApiService;
+
+    @Inject EventBus mEventBus;
+
+    @Inject
+    public ConverterTemperatureFactory(SharedPreferenceManager preferenceManager) {
+        this.mPreferenceManager = preferenceManager;
+    }
+
+    public ConvertTemperatureRepository getTemperatureConverter() {
+        if (mPreferenceManager.isConvertOnline()) {
+            return new OnlineConverterTemperature(mApiService, mEventBus);
+        } else {
+            return new OfflineConvertTemperature(mEventBus);
+        }
+    }
+
+}
