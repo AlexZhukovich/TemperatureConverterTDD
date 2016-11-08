ac69909154cd314a1195458dc1595a8d03aad11e
Alex <zhukovich.a@gmail.com>
2016-11-08 22:53:06 +0100

    Deleted unused import

--- app/src/androidTest/java/com/alexzh/temperatureconverter/TemperatureConverterActivityTest.java
+++ app/src/androidTest/java/com/alexzh/temperatureconverter/TemperatureConverterActivityTest.java
@@ -11,7 +11,6 @@ import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.View;
-import android.view.WindowManager;
 
 import com.alexzh.temperatureconverter.converter.TemperatureConverterActivity;
 
